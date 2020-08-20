6e5ce91b2ad5849db026677729a7b01114d40c54
JoshuaBPapa <joshua.b.papa@gmail.com>
2020-08-20 16:12:05 +0100

    Added break to switch cases

--- client/src/components/Priority/Priority.js
+++ client/src/components/Priority/Priority.js
@@ -2,15 +2,20 @@ import React from 'react';
 
 const Priority = ({ value }) => {
   let priorityString;
+  console.log(value)
   switch (value) {
     case 1:
       priorityString = 'severe';
+      break;
     case 2:
       priorityString = 'high';
+      break;
     case 3:
       priorityString = 'moderate';
+      break;
     case 4:
       priorityString = 'low';
+      break;
     default:
       priorityString = 'n/a';
   };
