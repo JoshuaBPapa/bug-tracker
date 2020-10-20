0a2c5f78bb7eb0b3ac9d1e6206a31de3580efec5
JoshuaBPapa <joshua.b.papa@gmail.com>
2020-10-20 12:33:32 +0100

    Added a hoc to handle authorisation on the client side

---
+++ client/src/hoc/withAuthLevelCheck.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import FeedbackMessage from '../components/FeedbackMessage/FeedbackMessage';
+
+const withAuthLevelCheck = (WrappedComponent, requiredAuthLevel) => {
+  // --- AUTH LEVELS --- //
+  // level 1 = user
+  // level 2 = project manager
+  // level 3 = admin
+  // level 4 = master admin
+
+  return class extends React.Component {
+    render() {
+      if (requiredAuthLevel > Number(localStorage.getItem('x-authorisation-level'))) {
+        return (
+          <FeedbackMessage>
+            You are not authorised to view this page.
+          </FeedbackMessage>
+        );
+      } else {
+        return (
+          <WrappedComponent />
+        );
+      }
+    };
+  };
+};
+
+export default withAuthLevelCheck;
