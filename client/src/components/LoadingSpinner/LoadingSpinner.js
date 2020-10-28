6442e02211e10dc9fcc942a3c0a921a3a10cd2d5
JoshuaBPapa <joshua.b.papa@gmail.com>
2020-10-28 13:12:50 +0000

    Added a loading spinner component to indicate loading

---
+++ client/src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './LoadingSpinner.css';
+
+const LoadingSpinner = () => (
+  <div class="lds-ring">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default LoadingSpinner;
