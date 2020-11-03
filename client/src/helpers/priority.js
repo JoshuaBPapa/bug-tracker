8fcf2873b37e5ddbac98d8762a33f87f08c1e390
JoshuaBPapa <joshua.b.papa@gmail.com>
2020-11-03 14:04:49 +0000

    Reorganised the levels of priority

--- client/src/helpers/priority.js
+++ client/src/helpers/priority.js
@@ -1,13 +1,13 @@
 export const convertPriorityToString = priority => {
   switch (priority) {
     case 1:
-      return 'Severe';
-    case 2:
-      return 'High';
-    case 3:
-      return 'Moderate';
-    case 4:
       return 'Low';
+    case 2:
+      return 'Moderate';
+    case 3:
+      return 'High';
+    case 4:
+      return 'Severe';
     default:
       return 'n/a';
   };
